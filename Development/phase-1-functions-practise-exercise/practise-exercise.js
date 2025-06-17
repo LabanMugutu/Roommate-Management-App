11217f40cf3d7f99ad3163d4af11ed9efb57e2dc
LabanMugutu <labanmugutu@gmail.com>
2025-06-17 16:32:10 +0300

    Finishes challenge

---
+++ Development/phase-1-functions-practise-exercise/practise-exercise.js
@@ -0,0 +1,19 @@
+function favouriteQuote(){
+    console.log("Dont do what can be done today tomorrow");
+}
+
+function introductionGreeting(name = "Nancy" , age = 25){
+    console.log(`Hi , I'm ${name} and I'm ${age} years old.`);
+}
+function areaOfARectangle(length , width){
+    return length * width
+}
+
+function evenOrOddNumber(number){
+    if (number %2 ===0){
+        return "Even"
+    }
+        else{
+            return "Odd"
+        }
+    }
