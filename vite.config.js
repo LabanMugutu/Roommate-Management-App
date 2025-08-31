872835f444c7a4f0e8773d20bf836b7a3532dcf0
Victorious Ngaruiya <victoriousngaruiya@gmail.com>
2025-08-31 20:19:26 +0300

    add index file

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // server: {
+  //   port: 3000,
+  //   open: true, // auto open browser
+  // },
+  // build: {
+  //   outDir: 'dist',
+  //   sourcemap: true,
+  // },
+})
